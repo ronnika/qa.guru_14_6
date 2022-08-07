b2606d2dca68d2fea807e6b19e2e25f1070f9244
ronnika <lunkestr@gmail.com>
2022-08-07 15:27:29 +0300

    added github Compare Plans test

---
+++ src/test/java/com/gitgub/GithubTests.java
@@ -0,0 +1,27 @@
+package com.gitgub;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class GithubTests {
+
+    @BeforeAll
+    static void configure() {
+        Configuration.baseUrl = "https://github.com/";
+        Configuration.holdBrowserOpen = true;
+    }
+
+    @Test
+    void comparePlansTest() {
+        open("");
+        $(byText("Pricing")).hover();
+        $(byText("Compare plans")).click();
+        $("h1").shouldHave(text("Choose the plan that’s right for you."));
+    }
+}
